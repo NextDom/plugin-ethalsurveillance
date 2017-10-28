94062968ca8eb6b57df8909e9bc609e6a95172d2
Ethal <developer@ethal-consulting.com>
2017-10-28 22:15:33 +0200

    improvement

--- desktop/php/ethalsurveillance.php
+++ desktop/php/ethalsurveillance.php
@@ -30,17 +30,13 @@ $date = array(
    </div>
 
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-    <legend><i class="fa fa-cog"></i> {{Gestion}}</legend>
-    <div class="eqLogicThumbnailContainer">
-      <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 140px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
+    <legend><i class="fa fa-table"></i> {{Mes equipements à surveiller}}</legend>
+  <div class="eqLogicThumbnailContainer">
+      <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
         <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
         <br>
         <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
     </div>
-    </div>
-    <legend><i class="fa fa-table"></i> {{Mes equipements à surveiller}}</legend>
-  <div class="eqLogicThumbnailContainer">
-
     <?php
 foreach ($eqLogics as $eqLogic) {
     $opacity = ($eqLogic->getIsEnable()) ? '' : jeedom::getConfiguration('eqLogic:style:noactive');
