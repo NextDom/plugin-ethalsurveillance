d91555ed3b756193b4945240b3a4795134521fd8
Ethal <developer@ethal-consulting.com>
2017-10-28 21:27:40 +0200

    improvement

--- desktop/php/ethalsurveillance.php
+++ desktop/php/ethalsurveillance.php
@@ -30,7 +30,7 @@ $date = array(
    </div>
 
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-    <legend>{{Gestion}}</legend>
+    <legend><i class="fa fa-cog"></i> {{Gestion}}</legend>
     <div class="eqLogicThumbnailContainer">
       <div class="cursor eqLogicAction" data-action="add" style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
         <i class="fa fa-plus-circle" style="font-size : 6em;color:#94ca02;"></i>
@@ -38,7 +38,7 @@ $date = array(
         <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
     </div>
     </div>
-    <legend>{{Mes equipements à surveiller}}</legend>
+    <legend><i class="fa fa-table"></i> {{Mes equipements à surveiller}}</legend>
   <div class="eqLogicThumbnailContainer">
 
     <?php
