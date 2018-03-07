51cd07bce933971dffed1e19488b4bf4392b9c15
Ethal <developer@ethal-consulting.com>
2018-03-07 16:11:03 +0100

    Update ethalsurveillance.class.php

--- core/class/ethalsurveillance.class.php
+++ core/class/ethalsurveillance.class.php
@@ -19,41 +19,6 @@
 /* * ***************************Includes********************************* */
 require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
 
-class ethalsurveillance extends eqLogic {
-    /*     * *************************Attributs****************************** */
-
-
-    /*     * ***********************Methode static*************************** */
-
-    /*
-     * Fonction exécutée automatiquement toutes les minutes par Jeedom */
-    public static function cron() {
-      foreach (eqLogic::byType('ethalsurveillance', true) as $eq) {
-        $equipementType = '';
-        $etat = $eq->ethGetValue('etat');
-        $pGeneral = $eq->getConfiguration('general','');
-        $configCmdEquipement = $eq->getConfiguration('cmdequipement','');
-<?php
-
-/* This file is part of Jeedom.
- *
- * Jeedom is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jeedom is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
- */
-
-/* * ***************************Includes********************************* */
-require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
-
 class ethalsurveillance extends eqLogic
 {
     /*     * *************************Attributs****************************** */
