46d0a120e37600f4158d9117c0a25774b0d44d62
Jules Lecoustre <38831521+JulesL2@users.noreply.github.com>
2018-06-06 21:41:19 +0200

    Update setCustomMDWarnings.sh

--- tests/tools/setCustomMDWarnings.sh
+++ tests/tools/setCustomMDWarnings.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-/*
- * This file is part of the NextDom software (https://github.com/NextDom or http://nextdom.github.io).
- * Copyright (c) 2018 NextDom.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 2.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+# This file is part of the NextDom software (https://github.com/NextDom or http://nextdom.github.io).
+# Copyright (c) 2018 NextDom.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 export MDLWAR="MD003,MD004,MD005,MD006,MD007,MD008,MD009,MD010,MD011,MD012,MD014,MD018,MD019,MD020,MD021,MD022,MD023,MD024,MD027,MD028,MD029,MD030,MD031,MD032,MD033,MD034,MD035,MD036,MD038,MD039"
